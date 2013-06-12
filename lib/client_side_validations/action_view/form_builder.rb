918a174355a3432544a8119500bba4c9804fe2d0
Otto Nordgren <otto.nordgren@gmail.com>
2013-06-12 11:20:13 +0200

    remove new_entities from validations

--- lib/client_side_validations/action_view/form_builder.rb
+++ lib/client_side_validations/action_view/form_builder.rb
@@ -97,6 +97,7 @@ module ClientSideValidations::ActionView::Helpers
         name = options[:name] || "#{@object_name}#{index}[#{method}]"
         child_index = @options[:child_index] ? "(\\d+|#{Regexp.escape(@options[:child_index].to_s)})" : "\\d+"
         #name = name.to_s.gsub(/_attributes\]\[#{child_index}\]/, '_attributes][]')
+        name = name.to_s.gsub(/_attributes\]\[new_entities\]/, '_attributes][]')
         name = "#{name}#{options[:multiple] ? "[]" : nil}"
         @options[:validators][@object][method] = { :name => name, :options => options[:validate] }
       end
